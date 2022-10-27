e35298eafc3b3f8a70b0161291ba56930882b686
Igor-UrbanowiczXD <urbanowiczigor95@gmail.com>
2022-10-28 01:23:16 +0200

    Zespołowe Srodowisko Programowania

--- Środowisko_Programowania/Lista_1/Zadanie3.sh
+++ Środowisko_Programowania/Lista_1/Zadanie3.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
+declare -A stats
 
+dir="$1"
 
-read -p 'Path: ' path
-ls $path
-read -p 'Word: ' word
-grep -irm 1 $word $path | wc -l
+for file in $(./Zadanie1.sh $dir); do
+    declare -A current_file
+    for word in $(cat $file); do
+        if [ ! ${current_file["$word"]+yup} ]; then
+            if [ ${stats["$word"]+yup} ]; then
+                ((stats['$word']++))
+            else
+                ((stats['$word']=1))
+            fi
+            current_file["$word"]="a"
+        fi
+    done
+    unset current_file
+done
+
+for word in "${!stats[@]}"; do
+    printf -- "\033[38;5;45mthe word ‘\033[0m$word\033[38;5;45m’ \033[38;5;45mappeared in ${stats[$word]} file(s)\033[0m\n"
+done
--- Środowisko_Programowania/Lista_1/Zadanie4.sh
+++ Środowisko_Programowania/Lista_1/Zadanie4.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
+#/bin/bash
+declare -A words_files
 
-for file in $path; do
-    
+dir="$1"
+for file in $(./Zadanie1.sh $dir); do
+    declare -A current_file
+    for word in $(cat $file); do
+        if [ ! ${current_file["$word"]+yup} ]; then
+            words_files["$word"]+=" $file"
+            current_file["$word"]="a"
+        fi
+    done
+    unset current_file
+done
+
+for word in "${!words_files[@]}"; do
+    printf -- "\033[38;5;45mthe word ‘\033[0m$word\033[38;5;45m’ \033[38;5;45mappeared in:\033[0m\n"
+    for file in ${words_files["$word"]}; do
+        printf "    \033[38;5;81ma file named ‘\033[0m$file\033[38;5;81m’:\033[0m\n"
+        grep -w -- "$word" "$file" | sed 's/^/    /'
+    done
+    printf "\n"
 done
